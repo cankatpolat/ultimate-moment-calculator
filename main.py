93c5d9dff1a1e69db851c87e62b88c77c598e9af
Cankat <cankat.17.polat@gmail.com>
2025-01-29 23:19:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-concrete = input("Concrete is C30 Concrete (Y/N)? :")
-steel = input("Steel is S420 Steel (Y/N)? :")
+try:
+    concrete = input("Concrete is C30 Concrete (Y/N)? :")
+    steel = input("Steel is S420 Steel (Y/N)? :")
+
+    if concrete == "Y" and steel == "Y":
+        areaSteel = float(input("Enter the area of steel (Ast) :"))
+
+    else print()
+
+
+except ValueError:
+    print("Answers must be only Y and N!")
